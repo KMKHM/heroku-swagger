a706f6e1176c465c5f0bd6690cc6791783c0b713
KMK_HM <kyun9151@gmail.com>
2022-07-11 00:36:46 +0900

    제발

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { app } from './src/app.js';
 import { db } from './src/db/config/db.config.js'
 
 const PORT = process.env.PORT || 8000;
-// db.connect(error => {
-//     if (error) throw error
-//     console.log("DB 연결 성공"); 
-// })
+db.connect(error => {
+     if (error) throw error
+     console.log("DB 연결 성공"); 
+})
 app.listen(process.env.PORT || 5000, () => {
     console.log(`서버 정상 실행 http://localhost:${PORT}`)
 })
