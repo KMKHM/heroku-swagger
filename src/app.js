63c172f32b42aa2c565421449bfc4ab8961d2f43
KMK_HM <kyun9151@gmail.com>
2022-07-11 16:10:08 +0900

    swagger 수정

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv'
-// const dotenv = require("dotenv");
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// import passport from 'passport';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs'
 
@@ -18,18 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// passportConfig();
-
-app.get("/", (req, res) => res.send("express!"));
-// app.use(passport.initialize());
-
-// app.use("/api/user", userRouter);
-// app.use("/api/study", studyRouter);
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
 
 app.use(
-  "/api-docs",
+  "/",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, { explorer: true })
 );
